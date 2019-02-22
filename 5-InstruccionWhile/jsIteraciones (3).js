4c1668919e935bc7cd088270ad33b40ef81c7e18
alvarojae <alvaroelena97@gmail.com>
2019-02-22 12:23:53 -0300

    alvaro

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,11 @@
 function mostrar()
 {
-
+var contador=0
 var clave = prompt("ingrese el número clave.");
-
+while(clave!="utn750" && contador<2)
+{
+    clave = prompt("ingrese la clave nuevamente");
+    console.log(++contador)
+}
 
 }//FIN DE LA FUNCIÓN
