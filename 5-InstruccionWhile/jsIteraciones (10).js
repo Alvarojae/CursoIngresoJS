ddc743c42e7b444297f32dd015348498e7dbf0ce
alvarojae <alvaroelena97@gmail.com>
2019-02-28 11:29:17 -0300

    alvaro

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,44 @@
 function mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	var contador;
+	var numeroPositivos;
+	var numeroNegativos;
+	var numeroIngresado;
+	var respuesta;
+	var bandera;
+	// declarar variables
+	respuesta='si';
+	contador=0;
+	bandera=0
+	while(respuesta!='no')
 	{
-		
 	
+		numeroIngresado=prompt("ingrese numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		if(bandera==0)
+		{
+		 numeroPositivos=numeroIngresado
+			numeroNegativos=numeroIngresado
+			bandera=1
+		
+		}else
+		{
+			if(numeroIngresado numeroPositivos)
+			{
+			 numeroPositivos=numeroIngresado;
+			}
+			if(numeroIngresado<numeroNegativos)
+			{
+				numeroNegativos=numeroIngresado;
+			}
+
+		}
+		
+		respuesta=prompt("ingrese no para salir");
 	}
+	console.log("el maximo es " numeroPositivos);
+	console.log("el minimo es "+numeroNegativos);
 
 
 
