8f1b768ab4047fedfa837b0327a39226ccd85231
alvarojae <alvaroelena97@gmail.com>
2019-02-28 10:22:32 -0300

    alvaro

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,4 +1,4 @@
-function mostrar()
+/*function mostrar()
 {
 
 	var contador;
@@ -10,6 +10,7 @@ function mostrar()
 	respuesta='si';
 	contador=0;
 	numeroMaximo=-9999;
+	numeroMinimo=9999;
 	while(respuesta!='no')
 	{
 		
@@ -19,9 +20,65 @@ function mostrar()
 		{
 			numeroMaximo=numeroIngresado;
 		}
+		if(numeroIngresado<numeroMinimo)
+		{
+			numeroMinimo=numeroIngresado;
+		}
 		respuesta=prompt("ingrese no para salir");
 	}
 	console.log("el maximo es "+numeroMaximo);
+	console.log("el minimo es "+numeroMinimo);
+
+
+
+}//FIN DE LA FUNCIÓN*/
+
+function mostrar()
+{
+
+	var contador;
+	var numeroMaximo;
+	var numeroMinimo;
+	var numeroIngresado;
+	var respuesta;
+	var bandera;
+	// declarar variables
+	respuesta='si';
+	contador=0;
+	//numeroMaximo=-9999;
+	//numeroMinimo=9999;
+	bandera=0
+	while(respuesta!='no')
+	{
+	
+		numeroIngresado=prompt("ingrese numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		if(bandera==0)
+		{
+
+			numeroMaximo=numeroIngresado
+			numeroMinimo=numeroIngresado
+			bandera=1
+			
+			
+
+		
+		}else{
+			if(numeroIngresado>numeroMaximo)
+			{
+				numeroMaximo=numeroIngresado;
+			}
+			if(numeroIngresado<numeroMinimo)
+			{
+				numeroMinimo=numeroIngresado;
+			}
+
+		}
+		
+		respuesta=prompt("ingrese no para salir");
+	}
+	console.log("el maximo es "+numeroMaximo);
+	console.log("el minimo es "+numeroMinimo);
 
 
 
