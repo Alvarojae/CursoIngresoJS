f0c3b3777b4945ad6a24ecd351d1bb40e6dadd47
alvarojae <alvaroelena97@gmail.com>
2019-03-04 21:05:40 -0300

    alvaro

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -2,12 +2,62 @@ function mostrar()
 {
 var notas;
 var sexo;
-var
+var promedioNotas;
+var notabaja;
+var sexomenor;
+var contadorvarones;
+var sumaNotas;
 //variables
-notas=0
+notas=0;
+alumnos=0;
+promedioNotas=0;
+notabaja=11;
+contadorvarones=0;
+sumaNotas=0;
 
 
 //definir variables
-notas=prompt("ingrese nota")
-notas=parseInt(notas) 
+
+
+
+while(alumnos<5)
+{
+    notas=prompt("ingrese nota")
+    notas=parseInt(notas) 
+    sexo=prompt("Sexo f o m?")
+
+while(notas<0 || notas>10)
+{
+    notas=prompt("ingrese nota correcta(entre 0 y 10)")
+    notas=parseInt(notas) 
+}
+while(sexo!="f" && sexo!="m")
+{
+    sexo=prompt("ingrese el sexo correctamente(f o m)")
+    
+}
+
+if(notas>0)
+{
+    sumaNotas=notas+sumaNotas
+}
+if(notas<notabaja)
+{
+notabaja=notas
+sexomenor=sexo
+}
+if(notas>5 && sexo=="m")
+{
+    contadorvarones++
+}
+
+alumnos++
+}
+promedioNotas=sumaNotas/alumnos
+alert("el promedio de notas totales es : " + promedioNotas)
+alert("la nota mas baja es : " + notabaja + " y el sexo de la nota es: " + sexomenor)
+alert("la cantidad de varones que su nota fue mayor a 6 fue: " + contadorvarones)
+
+
+
 }
